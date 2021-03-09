3ccb8272499470b8a8bc420ede4b649e8da17b26
PeterKrizan <krizan.peter.fiit@gmail.com>
2021-03-09 17:37:09 +0100

    Added sysout

--- src/application/Main.java
+++ src/application/Main.java
@@ -15,6 +15,7 @@ public class Main extends Application {
 			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
 			primaryStage.setScene(scene);
 			primaryStage.show();
+			System.out.println("nejaka zmena");
 		} catch(Exception e) {
 			e.printStackTrace();
 		}
